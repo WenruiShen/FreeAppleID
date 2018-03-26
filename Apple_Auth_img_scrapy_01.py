d53b3cc71bf580a8023d31952f4e290788171729
shenwenrui <Mobike_swr123>
2018-03-27 00:47:05 +0800

    use selenium + Chrome.

---
+++ Apple_Auth_img_scrapy_01.py
@@ -0,0 +1,29 @@
+##############################################
+#
+# Author:       Shen Wenrui
+# Date:         20180326
+# Description:
+#
+##############################################
+
+import sys
+from selenium import webdriver
+
+appleIdHomepageUrl = 'https://appleid.apple.com'
+signPageUrl = '/#!&page=signin'
+
+
+#help(webdriver)
+browser = webdriver.Chrome()
+
+# Apple-Id Homepage Signin
+#signinUrl = appleIdHomepageUrl + signPageUrl
+signinUrl = 'http://www.baidu.com'
+print(signinUrl)
+browser.get(signinUrl)
+html = browser.page_source
+
+
+
+#browser.quit()
+#exit()
