861216bda7f4819b3a22c87621b914d5cb158dbf
shenwenrui <Mobike_swr123>
2018-03-27 18:45:17 +0800

    wrap apple account page in format of base64 successfully

--- Apple_Auth_img_scrapy_01.py
+++ Apple_Auth_img_scrapy_01.py
@@ -26,6 +26,8 @@ browser = webdriver.Chrome()
 
 testUrl = appleIdSignUpPageUrl
 print(testUrl)
+
+browser.implicitly_wait(5)
 browser.get(testUrl)
 html = browser.page_source
 #print(html)
@@ -33,15 +35,13 @@ html = browser.page_source
 authImgBase64_xpath = "/html/body/div[@id='content']/aid-web/div[@class='app-container']/div[@id='app-content']/" \
                     "div[@id='flow']/create-app/aid-create//div[@class='idms-flow-container']//div[@class='idms-step-content']/" \
                     "div/div/div[6]/div/create-captcha/" \
-                    "div/div/div/div/div[1]/div/idms-captcha/div"#/img"
+                    "div/div/div/div/div[1]/div/idms-captcha/div/img"
 
 authImgElement = browser.find_element_by_xpath(authImgBase64_xpath)
 
-#authImgBase64 = authImgElement.get_attribute('src')
-print('authImgElement is: ' + str(authImgElement))
-#print('authImgBase64 is: ' + str(authImgBase64))
-
-
+print('authImgElement is: ' + str(authImgElement.get_attribute('innerHTML')))
+authImgBase64 = authImgElement.get_attribute('src')
+print('authImgBase64 is: ' + str(authImgBase64))
 
 browser.quit()
 exit()
