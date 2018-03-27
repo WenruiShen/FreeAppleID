bab72a5e7df7529530e2413cbd025688043fea42
shenwenrui <Mobike_swr123>
2018-03-27 20:34:20 +0800

    sync

--- Apple_Auth_img_scrapy_01.py
+++ Apple_Auth_img_scrapy_01.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 ##############################################
 #
 # Author:       Shen Wenrui
@@ -10,26 +11,21 @@ import sys
 from selenium import webdriver
 
 baiduUrl = 'http://www.baidu.com'
-
 appleHomepageUrl = 'https://www.apple.com/cn/'
 # Apple-Id Signin page
 appleIdHomePageUrl = 'https://appleid.apple.com'
-#signPageUrl = '/#!&page=signin'
-#signinUrl = appleIdHomepageUrl + signPageUrl
 
 # Apple-Id Signup page
 appleIdSignUpPageUrl = appleIdHomePageUrl + '/account'
 
-
 browser = webdriver.Chrome()
-
 
 testUrl = appleIdSignUpPageUrl
 print(testUrl)
 
 browser.implicitly_wait(5)
 browser.get(testUrl)
-html = browser.page_source
+#html = browser.page_source
 #print(html)
 
 authImgBase64_xpath = "/html/body/div[@id='content']/aid-web/div[@class='app-container']/div[@id='app-content']/" \
@@ -39,9 +35,20 @@ authImgBase64_xpath = "/html/body/div[@id='content']/aid-web/div[@class='app-con
 
 authImgElement = browser.find_element_by_xpath(authImgBase64_xpath)
 
-print('authImgElement is: ' + str(authImgElement.get_attribute('innerHTML')))
+#print('authImgElement is: ' + str(authImgElement.get_attribute('innerHTML')))
+
 authImgBase64 = authImgElement.get_attribute('src')
-print('authImgBase64 is: ' + str(authImgBase64))
+print(authImgBase64)
+
+import base64
+authImgStr = base64.b64decode(authImgBase64[len('data:image/jpeg;base64, '):])
+authImg_f = open("001.jpeg", "wb")
+authImg_f.write(authImgStr)
+authImg_f.close()
+
+#import PIL.Image
+#im = PIL.Image.open('001.jpeg')
+#im.show()
 
 browser.quit()
 exit()
