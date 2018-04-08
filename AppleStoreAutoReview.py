dcec261c292aff4f60c8c969f08e38eca5f9dff3
shenwenrui <Mobike_swr123>
2018-04-09 00:20:16 +0800

    成功登陆AppStore,开始进行Review.

--- AppleStoreAutoReview.py
+++ AppleStoreAutoReview.py
@@ -21,8 +21,51 @@ scpt = applescript.AppleScript('''
     on Baz(x, y)
         return x * y
     end bar
+    
+    -- Function for iTunes sign in
+    property loginBtn : "登录…"
+    property logoutBtn : "注销"
+    -- Launch iTunes
+    
+    
+    on openAppStore (appleId, passwd)
+        tell application "App Store" to activate
+        tell application "System Events"
+            tell process "App Store"
+                set frontmost to true
+                
+                try
+                    click menu item loginBtn of menu "商店" of menu bar 1
+                on error
+                    click menu item logoutBtn of menu "商店" of menu bar 1
+                    delay 2
+                    click menu item loginBtn of menu "商店" of menu bar 1
+                end try
+                
+                delay 2
+                set value of text field 2 of sheet 1 of window "App Store" to appleId
+                delay 1
+                set value of text field 1 of sheet 1 of window "App Store" to passwd
+                
+                delay 2
+                keystroke return      -- Press the return key "登陆"
+                delay 5
+                --keystroke return      -- Click Review
+                click menu item "Review"
+                
+                --keystroke appleId
+                --keystroke tab
+                delay 1
+                --keystroke passwd
+                keystroke return      -- Press the return key
+			
+            end tell
+        end tell
+        
+        return True
+    end openAppStore
 ''')
 
-print(scpt.run('Hello', 'World')) #-> None
-print(scpt.call('foo')) #-> "bar"
-print(scpt.call('Baz', 3, 5)) #-> 15
+user_email = "zhangtao001@protonmail.com"
+user_password = "Apple_swr123"
+print(scpt.call('openAppStore', user_email, user_password))
