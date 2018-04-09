6496095a77f79fd51858cde7e9bcfb8f9f6c61b7
shenwenrui <Mobike_swr123>
2018-04-09 20:44:58 +0800

    成功选择None为支付方式，激活成功。

--- AppleStoreAutoReview.py
+++ AppleStoreAutoReview.py
@@ -21,7 +21,7 @@ scpt = applescript.AppleScript('''
         tell application "System Events"
             tell process "App Store"
                 set frontmost to true
-                delay 3
+                delay 10
                 try
                     click menu item 12 of menu 1 of menu bar item 4 of menu bar 1 -- "Sign In…"
                 on error
@@ -30,7 +30,7 @@ scpt = applescript.AppleScript('''
                     click menu item 12 of menu 1 of menu bar item 4 of menu bar 1 -- "Sign In…"
                 end try
                 
-                delay 1
+                delay 10
                 set value of text field 2 of sheet 1 of window "App Store" to appleId
                 set value of text field 1 of sheet 1 of window "App Store" to passwd
                 
@@ -55,6 +55,17 @@ scpt = applescript.AppleScript('''
                 click menu item 2 of menu 1 of group 1 of window "App Store"
                 --press continue
                 click button 3 of group 7 of UI element 1 of scroll area 1 of group 1 of group 1 of window "App Store"
+                
+                delay 10
+                --select 'None' as the payment method
+                click radio button 6 of group 6 of UI element 1 of scroll area 1 of group 1 of group 1 of window "App Store"
+                --press continue
+                delay 1
+                click button 3 of group 14 of UI element 1 of scroll area 1 of group 1 of group 1 of window "App Store"
+                
+                delay 10
+                --Welcom page -> Continue
+                click button 1 of sheet 1 of window "App Store"
                 
             end tell
         end tell
