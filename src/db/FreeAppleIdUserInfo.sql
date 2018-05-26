c9bfbc8291620a12c216ab702e3ccff5802ebc90
shenwenrui <Mobike_swr123>
2018-05-26 23:29:15 +0800

    开始添加数据库

---
+++ src/db/FreeAppleIdUserInfo.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS FreeAppleIdDB;
+USE FreeAppleIdDB;
+
+CREATE TABLE FreeAppleIdUserInfo
+(
+Id_P int NOT NULL PRIMARY KEY,
+userEmail varchar(255) NOT NULL UNIQUE,
+applePassword varchar(255) NOT NULL,
+
+LastName varchar(255),
+FirstName varchar(255),
+BirthDay date,
+Address varchar(255),
+City varchar(255)
+);
---
+++ src/db/userModels.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+#coding=utf-8
+
+##############################################
+#
+# Author:       Shen Wenrui
+# Date:         20180526
+# Description:
+#
+##############################################
+
+#import pymysql
+import mysql.connector
+
+# 打开数据库连接
+#db = pymysql.connect('localhost', 'freeapple', 'dbfreeid2018', 'FreeAppleIdDB')
+db = mysql.connector.connect(user='freeapple',
+                             password='dbfreeid2018',
+                             db='FreeAppleIdDB',
+                             host='127.0.0.1',
+                             port=3306,
+                             charset='utf8')
+
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+
+# 使用 execute()  方法执行 SQL 查询
+cursor.execute("SELECT VERSION()")
+
+# 使用 fetchone() 方法获取单条数据.
+data = cursor.fetchone()
+
+print("Database version : %s " % data)
+
+# 关闭数据库连接
+db.close()
+
